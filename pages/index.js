d15c5cd971351ce1b998fffc9904ccf97aa3114e
Re9iNee <attarzadeh76@gmail.com>
2021-08-16 15:11:20 +0430

    added about me in blog section

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>The name is Reza and Im try learning new frameworks for react development</p>
         <p>
           (This is a sample webiste - you'll be building a site like this on{' '})
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
